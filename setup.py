881f9cb7a26d66e8685977ea4e47c2487d5ad386
ctSkennerton <c.skennerton@gmail.com>
2013-03-18 16:54:38 +1000

    Add in uniqueMarkers.py to bin directory in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='checkM',
-    version='0.2.0',
+    version='0.3.1',
     author='Michael Imelfort',
     author_email='mike@mikeimelfort.com',
     packages=['metachecka2000', 'metachecka2000.test'],
-    scripts=['bin/metachecka2000', 'bin/checkM'],
+    scripts=['bin/metachecka2000', 'bin/checkM', 'bin/uniqueMarkers.py'],
     url='http://pypi.python.org/pypi/MetaChecka2000/',
     license='GPL3',
     description='Beta check your meta before you wreck your meta',
