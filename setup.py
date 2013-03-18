f92ce52d47804acc6568202f07d2881314fe59bb
ctSkennerton <c.skennerton@gmail.com>
2013-03-18 22:50:56 +1000

    Changed README file extension to '.md' for proper html conversion

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     url='http://pypi.python.org/pypi/MetaChecka2000/',
     license='GPL3',
     description='Beta check your meta before you wreck your meta',
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
     install_requires=[
                       'simplehmmer >= 0.2.2',
                       'biopython >= 1.58'
