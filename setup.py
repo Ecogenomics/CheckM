75d577423649423628834d2f7e3b551749638782
dparks1134 <donovan.parks@gmail.com>
2014-02-12 12:23:42 +1000

    Modified CheckM install details.

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from distutils.core import setup
 
 setup(
     name='checkm-ACE',
-    version='1.0.0',
+    version='0.5.0',
     author='Donovan Parks, Michael Imelfort, Connor Skennerton',
     author_email='donovan.parks@gmail.com',
     packages=['checkm', 'checkm.plot', 'checkm.test', 'checkm.lib'],
     scripts=['bin/checkm'],
     url='http://pypi.python.org/pypi/checkm/',
     license='GPL3',
-    description='Estimate completeness and contamination of putative genomes.',
+    description='Assess the quality of putative genome bins.',
     long_description=open('README.md').read(),
     install_requires=[
-        "numpy >= 1.6.1",
-        "scipy >= 0.10.1",
+        "numpy >= 1.8.0",
+        "scipy >= 0.9.0",
         "matplotlib >= 1.1.0",
         "pysam >= 0.7.4",
-        "sqlite3 >= 2.6.0"],
+        "dendropy >= 3.13.0"],
 )
