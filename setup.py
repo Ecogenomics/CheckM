3a7d5c430d0f16f20d13784ffdf332a1e496893a
Donovan Parks <donovan.parks@gmail.com>
2019-11-07 14:38:18 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+#!/usr/bin/env python3
 
 import os
-
+from setuptools import setup
 
 def version():
     setupDir = os.path.dirname(os.path.realpath(__file__))
@@ -19,6 +19,13 @@ setup(
     url='http://pypi.python.org/pypi/checkm/',
     license='GPL3',
     description='Assess the quality of putative genome bins.',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
     install_requires=[
         "numpy >= 1.13.1",
         "scipy >= 0.19.1",
