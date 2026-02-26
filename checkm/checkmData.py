242ed4c294b91ee34893c1216bb6e5150acb9dea
meereeum <meereeum@gmail.com>
2026-02-25 17:53:48 -0800

    cross-compatibility w/ old and new setuptools
    per migration strategy @
    https://importlib-resources.readthedocs.io/en/latest/migration.html#pkg-resources-resource-filename

--- checkm/checkmData.py
+++ checkm/checkmData.py
@@ -22,7 +22,18 @@
 import os
 import sys
 import logging
-from pkg_resources import resource_filename
+
+try: # setuptools<81
+    from pkg_resources import resource_filename
+    def get_resource(resource, pkg='checkm'):
+        return os.path.abspath(resource_filename(pkg, resource))
+
+except(ModuleNotFoundError): # setuptools>=81
+    import importlib_resources
+    def get_resource(resource, pkg='checkm'):
+        with importlib_resources.as_file(pkg) / resource as f:
+            return str(f)
+
 import json
 from collections import namedtuple
 
@@ -121,8 +132,7 @@ class DBManager(mm.ManifestManager):
         else:
             # read data path from DATA_CONFIG file
             if configFile is None:
-                configFile = os.path.abspath(
-                    resource_filename('checkm', 'DATA_CONFIG'))
+                configFile = get_resource('DATA_CONFIG')
 
             self.config = DBConfig(configFile=configFile)
             self.type = self.config.values["manifestType"]
